82571d9d949491e1428f5a84430fd762be5a11ef
li <2530527474@qq.com>
2021-09-16 23:57:21 +0800

    feat: init project

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch("/api/date");
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>Deployed with 4everland!</h2>
-      <p>
-        This project was bootstrapped with{" "}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{" "}
-        and contains three directories, <code>/public</code> for static assets,{" "}
-        <code>/src</code> for components and content, and <code>/api</code>{" "}
-        which contains a serverless <a href="https://golang.org/">Go</a>{" "}
-        function. See{" "}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : "Loading date..."}</p>
-    </main>
+      </header>
+    </div>
   );
 }
 
